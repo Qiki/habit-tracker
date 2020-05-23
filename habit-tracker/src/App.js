a3dfe2b50923c4146f382b74ac58906ab6ca7a9c
Qiki <wangqi.zoro@gmail.com>
2020-05-23 12:23:13 -0400

    show ui base on mock data

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -1,18 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mockData: [
+        {
+          name: 'Neil',
+          date: '05/02/2020',
+          yes: 1,
+        },
+        {
+          name: 'Neil',
+          date: '05/02/2020',
+          yes: 0,
+        },
+        {
+          name: 'Qi',
+          date: '05/02/2020',
+          yes: 1,
+        },
+        {
+          name: 'Neil',
+          date: '05/03/2020',
+          yes: 0,
+        },
+        {
+          name: 'Qi',
+          date: '05/03/2020',
+          yes: 1,
+        },
+        {
+          name: 'Neil',
+          date: '05/04/2020',
+          yes: 0,
+        },
+
+        {
+          name: 'Qi',
+          date: '05/04/2020',
+          yes: 1,
+        },
+        {
+          name: 'Neil',
+          date: '05/04/2020',
+          yes: 1,
+        },
+        {
+          name: 'Qi',
+          date: '05/04/2020',
+          yes: 1,
+        },
+      ]
+    };
+  }
+
+  renderTable(name) {
+    let data =  this.state.mockData.filter(item => item.name === name);
+
+    return data.map(info => {
+      const {date, yes} = info;
+      return(
+        <tr>
+          <td>{date}</td>
+          <td>{yes}</td>
+        </tr>
+      ); 
+    })
+  }
+
+  renderTableHeader() {
+    return (
+      <tr>
+        <th>Date</th>
+        <th>Yes</th>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Qi and Neil's Habit Tracker</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="dashboard">
+          <div class="left">
+            <div class="name">Neil</div>
+            <div class="exercise-table">
+              <table>
+                {this.renderTableHeader()}
+                {this.renderTable("Neil")}
+              </table>
+            </div>
+          </div>
+          <div class="right">
+            <div class="name">Qi</div>
+            <div class="exercise-table">
+              <table>
+                {this.renderTableHeader()}
+                {this.renderTable("Qi")}
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
